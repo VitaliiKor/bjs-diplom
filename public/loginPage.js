5ded105b48cfa982cecf9a61b2a0655c29957b0b
VitaliiKor <me8@yandex.ru>
2021-12-11 06:27:05 +0700

    Первая отправка на проверку. Файл loginPage

---
+++ public/loginPage.js
@@ -0,0 +1,26 @@
+"use strict";
+let userForm = new UserForm();
+
+userForm.loginFormCallback = (data) => {
+    ApiConnector.login(data, (response)=>{
+        if (response.success === true) {
+            location.reload();
+        }
+        else {
+            userForm.setLoginErrorMessage(`Произошла ошибка: ${response.error}`);
+        }
+    });
+};
+
+userForm.registerFormCallback = (data) => {
+    ApiConnector.register(data, (response) => {
+       if (response.success === true) {
+           userForm.id = response.id;
+           location.reload();
+       }
+       else {
+           userForm.setRegisterErrorMessage(`Произошла ошибка регистрации пользователя ${data.login}: ${response.error}`);
+       }
+    });
+}
+
